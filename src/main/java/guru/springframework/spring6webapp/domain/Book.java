540996e43bc737ef130fcbe2ec709e7b924143b9
Clifford Bechtel <crbechtel@gmail.com>
2023-02-21 14:12:04 -0500

    Update Book.java

--- src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -13,11 +13,12 @@ import java.util.Set;
 public class Book {
 
     @Id
-
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String title;
     private String isbn;
 
+    @ManyToOne
     private Publisher publisher;
 
     @ManyToMany
